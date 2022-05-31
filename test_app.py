be6d949c6ca9e0045e7a1a9de678707d0fed4cf4
AnaghaDAnanth <63648277+AnaghaDAnanth@users.noreply.github.com>
2022-05-31 17:48:18 +0530

    Delete test_app.py

--- test_app.py
+++
@@ -1,8 +0,0 @@
-from myapp import app
-
-
-def test_hello():
-    response = app.test_client().get('/')
-
-    assert response.status_code == 200
-    assert response.data == b'Welcome to Flask APP'
